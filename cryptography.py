fa8d7799b5b87f83387c234e6a16a275cb3e9c0a
dina-hertog <dinahertog-raz@hanovernorwichschools.org>
2015-10-23 14:37:14 -0400

    Updated from Brython Server: 10/23/2015 2:37:14 PM

--- cryptography.py
+++ cryptography.py
@@ -1,6 +1,6 @@
 """
 cryptography.py
-Author: <your name here>
+Author: Dina
 Credit: <list sources used, if any>
 
 Assignment:
@@ -10,3 +10,8 @@ Write and submit a program that encrypts and decrypts user data.
 See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
 """
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
+
+command = input("Enter e to encrypt, d to decrypt, or q to quit: ")
+while command != 'e' and command != 'd' and command != 'q':
+    print("Did not understand command, try again.")
+    command = input("Enter e to encrypt, d to decrypt, or q to quit: ")
