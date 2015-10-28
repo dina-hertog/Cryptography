c4edb77b83e51a0bb41be1d2ce05ece46647dbdd
dina-hertog <dinahertog-raz@hanovernorwichschools.org>
2015-10-28 13:37:51 -0400

    Updated from Brython Server: 10/28/2015 1:37:51 PM

--- cryptography.py
+++ cryptography.py
@@ -13,9 +13,9 @@ associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .
 command = input("Enter e to encrypt, d to decrypt, or q to quit: ")
 
 while command != 'q':
-    while command != 'e' and command != 'd' and command != 'q':
+    if command != 'e' and command != 'd':
         command = input("Did not understand command, try again. ")
-    if command == 'e':
+    elif command == 'e':
         message = input("Message: ")
         key = input("Key: ")
         for x in message:
@@ -33,8 +33,25 @@ while command != 'q':
                 knum = associations.find(y)
             lnum = mnum-knum
             print(associations[lnum], end = '')
-    #command = input("Enter e to encrypt, d to decrypt, or q to quit: ")
+    command = input("Enter e to encrypt, d to decrypt, or q to quit: ")
 print("Goodbye! ")
-
-
-
+"""
+if command == 'e':
+        message = input("Message: ")
+        key = input("Key: ")
+        for x in message:
+            mnum = associations.find(x)
+            for y in key:
+                knum = associations.find(y)
+            lnum = mnum+knum
+            print(associations[lnum], end = '')
+else:
+        message = input("Message: ")
+        key = input("Key: ")
+        for x in message:
+            mnum = associations.find(x)
+            for y in key:
+                knum = associations.find(y)
+            lnum = mnum-knum
+            print(associations[lnum], end = '')
+"""
