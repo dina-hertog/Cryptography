2db362dd525fca956bd254690dd251aef1a1378f
dina-hertog <dinahertog-raz@hanovernorwichschools.org>
2015-11-02 14:42:35 -0500

    Updated from Brython Server: 11/2/2015 2:42:35 PM

--- cryptography.py
+++ cryptography.py
@@ -12,33 +12,32 @@ See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptog
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
 command = input("Enter e to encrypt, d to decrypt, or q to quit: ")
 
-while command != 'q':
-    while command != 'e' and command != 'd' and command != 'q':
-        command = input("Did not understand command, try again. ")
-    if command == 'e':
-        message = input("Message: ")
-        key = input("Key: ")
-        while len(key) < len(message):
-            key = key+key
-        km = list(zip(message, key))
-        for a in km:
-            mnum = associations.find(a[0])
-            knum = associations.find(a[1])
-            b = mnum+knum
-            print(associations[b], end = '')
-    elif command == 'd':
-        message = input("Message: ")
-        key = input("Key: ")
-        while len(key) < len(message):
-            key = key+key
-        km = list(zip(message, key))
-        for a in km:
-            mnum = associations.find(a[0])
-            knum = associations.find(a[1])
-            b = mnum-knum
-            print(associations[b], end = '')
-    else:
-        print("Goodbye! ")
-    print()
+while command != 'e' and command != 'd' and command != 'q':
+    command = input("Did not understand command, try again. ")
+if command == 'e':
+    message = input("Message: ")
+    key = input("Key: ")
+    while len(key) < len(message):
+        key = key+key
+    km = list(zip(message, key))
+    for a in km:
+        mnum = associations.find(a[0])
+        knum = associations.find(a[1])
+        b = mnum+knum
+        print(associations[b], end = '')
+elif command == 'd':
+    message = input("Message: ")
+    key = input("Key: ")
+    while len(key) < len(message):
+        key = key+key
+    km = list(zip(message, key))
+    for a in km:
+        mnum = associations.find(a[0])
+        knum = associations.find(a[1])
+        b = mnum-knum
+        print(associations[b], end = '')
+else:
+    print("Goodbye! ")
+print()
+if command != 'q':
     command = input("Enter e to encrypt, d to decrypt, or q to quit: ")
-print("Goodbye! ")
