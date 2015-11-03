9039bf0b3440b4944c6d5b3200e34df03530377f
dina-hertog <dinahertog-raz@hanovernorwichschools.org>
2015-11-03 14:19:39 -0500

    Updated from Brython Server: 11/3/2015 2:19:39 PM

--- cryptography.py
+++ cryptography.py
@@ -11,25 +11,30 @@ See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptog
 """
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
 command = input("Enter e to encrypt, d to decrypt, or q to quit: ")
-
-while command != 'e' and command != 'd' and command != 'q':
-    command = input("Did not understand command, try again. ")
-if command == 'e' or command == 'd':
-    message = input("Message: ")
-    key = input("Key: ")
-    while len(key) < len(message):
-        key = key+key
-    km = list(zip(message, key))
-    for a in km:
-        mnum = associations.find(a[0])
-        knum = associations.find(a[1])
-        if command == 'e':
-            b = mnum + knum
-        else:
-            b = mnum - knum
-        print(associations[b], end = '')
-else:
-    print("Goodbye! ")
-print()
-if command != 'q':
-    command = input("Enter e to encrypt, d to decrypt, or q to quit: ")
+while command != 'q':
+    while command != 'e' and command != 'd' and command != 'q':
+        command = input("Did not understand command, try again. ")
+    if command == 'e' or command == 'd':
+        message = input("Message: ")
+        key = input("Key: ")
+        while len(key) < len(message):
+            key = key+key
+        km = list(zip(message, key))
+        for a in km:
+            mnum = associations.find(a[0])
+            knum = associations.find(a[1])
+            if command == 'e':
+                b = mnum + knum
+                if b > len(associations):
+                    b = b-len(associations)
+            else:
+                b = mnum - knum
+                if b < 0:
+                    b = b + len(associations)
+            print(associations[b], end = '')
+    else:
+        print("Goodbye! ")
+    print()
+    if command != 'q':
+        command = input("Enter e to encrypt, d to decrypt, or q to quit: ")
+print("goodbye! ")
